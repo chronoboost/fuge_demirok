59c78c4c0c3151eec29b26d5d14bdb317a196a1f
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-06-26 12:49:59 -0700

    Add basic footer and link code for fonts

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
-fetch('data/FD-catalog.json')
-    .then(response => response.json())
-    .then(data => {
-        const firstItem = data[62]; 
-        const itemContent = `
-            <h2>${firstItem.Title}</h2>
-            <img src="/data/downloaded_images/${firstItem.ImagePaths[0]}" alt="${firstItem.Title}">
-            <p>${firstItem.Description}</p>
-            <p>Artist: ${firstItem.Artist}</p>
-            <p>Type: ${firstItem.Type}</p>
-        `;
-        document.getElementById('gallery-preview').innerHTML = itemContent;
-    })
-    .catch(error => {
-        console.error('Error fetching the catalog:', error);
-    });
+// fetch('data/FD-catalog.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         const firstItem = data[62]; 
+//         const itemContent = `
+//             <h2>${firstItem.Title}</h2>
+//             <img src="/data/downloaded_images/${firstItem.ImagePaths[0]}" alt="${firstItem.Title}">
+//             <p>${firstItem.Description}</p>
+//             <p>Artist: ${firstItem.Artist}</p>
+//             <p>Type: ${firstItem.Type}</p>
+//         `;
+//         document.getElementById('gallery-preview').innerHTML = itemContent;
+//     })
+//     .catch(error => {
+//         console.error('Error fetching the catalog:', error);
+//     });
