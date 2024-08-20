396df9d886535e55d8fd05200f29d7b299be1549
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-08-20 16:11:53 -0700

    Add boilerplate to pages, make header a script component to inject on each page, fix footer styling, flesh out about page with copy

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector('header');
+    header.innerHTML = `
+        <div class="header-div">
+        <a class="header-title" href="/index.html"><h1>Füge Demirok</h1></a>
+        </div>
+        <nav class="navbar">
+        <ul class="navbar-items">
+        <li><a class="navbar-item first-item" href="/pages/sculpture.html">Sculpture</a></li>
+        <li><a class="navbar-item" href="/pages/painting.html">Painting</a></li>
+        <li><a class="navbar-item" href="/pages/multimedia.html">Multimedia</a></li>
+        <li><a class="navbar-item" href="/pages/print.html">Print</a></li>
+        <li><a class="navbar-item last-item" href="/pages/about.html">About</a></li>
+        </ul>
+        </nav>
+    `;
+})
 
 
 document.addEventListener("DOMContentLoaded", function() {
