9c7269fa0ef25d3d0a145c8f51ef6a7304bd0673
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-08-22 12:06:40 -0700

    add script to render sculpture gallery with link to details page

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
         </div>
         <nav class="navbar">
         <ul class="navbar-items">
-        <li><a id="sculpture" class="navbar-item first-item" href="/pages/sculpture.html">Sculpture</a></li>
-        <li><a id="painting" class="navbar-item" href="/pages/painting.html">Painting</a></li>
-        <li><a id="multimedia" class="navbar-item" href="/pages/multimedia.html">Multimedia</a></li>
-        <li><a id="print" class="navbar-item" href="/pages/print.html">Print</a></li>
-        <li><a id="about" class="navbar-item last-item" href="/pages/about.html">About</a></li>
+        <li><a id="Sculpture" class="navbar-item first-item" href="/pages/sculpture.html">Sculpture</a></li>
+        <li><a id="Painting" class="navbar-item" href="/pages/painting.html">Painting</a></li>
+        <li><a id="Multimedia" class="navbar-item" href="/pages/multimedia.html">Multimedia</a></li>
+        <li><a id="Print" class="navbar-item" href="/pages/print.html">Print</a></li>
+        <li><a id="About" class="navbar-item last-item" href="/pages/about.html">About</a></li>
         </ul>
         </nav>
     `;
@@ -32,3 +32,24 @@ document.addEventListener("DOMContentLoaded", function() {
         <p>&copy; ${currentYear}</p>
     `;
 })
+
+
+fetch('/data/FD-catalog.json')
+    .then(response => response.json())
+    .then(data => {
+        const sculptureItems = data.filter(item => item.Type === "Sculpture");
+        const itemContent = sculptureItems.map(item => {
+            return `
+                <div class="gallery-grid-img">
+                    <a href="/pages/details.html?id=${item.ID}"><img src="/data/downloaded_images/${item.ImagePaths[0]}" alt="${item.Title}"></a>
+                </div>
+            `;
+        }).join('');
+        const gallery = document.getElementById('sculpture-gallery');
+        if (gallery) {
+            gallery.innerHTML = itemContent;
+        };
+    })
+    .catch(error => {
+    console.error('Error fetching the catalog:', error);
+});
