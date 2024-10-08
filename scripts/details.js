70875a4651b0a1d9017d7524b47c525618e62620
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-10-08 11:05:52 -0700

    Add changes to wrap up basic structure, styling, and functionality of site. Ready for first launch

--- scripts/details.js
+++ scripts/details.js
@@ -11,16 +11,22 @@ fetch('/data/FD-catalog.json')
             let detailsDiv = document.getElementById('item-details');
 
             // images
+            let detailsImgDiv = document.createElement('div');
+            detailsImgDiv.classList.add('details-img-div');
+
             item.ImagePaths.forEach(path => {
                 let detailsImg = document.createElement('img');
                 detailsImg.setAttribute('src', `/data/downloaded_images/${path}`);
                 detailsImg.setAttribute('alt', `${item.Title}, a ${item.Type}`);
-                detailsImg.setAttribute('style', 'max-height: 900px;margin-bottom: 1rem;');
+                detailsImg.setAttribute('style', 'max-height: 900px;max-width: 1080px;margin-bottom: 1rem;');
                 
-                detailsDiv.appendChild(detailsImg);
+                detailsImgDiv.appendChild(detailsImg);
             });
 
+            detailsDiv.appendChild(detailsImgDiv);
+
             let detailsSticky = document.createElement('div');
+            detailsSticky.classList.add('details-sticky');
 
             // title
             let detailsTitle = document.createElement('h3');
@@ -29,19 +35,13 @@ fetch('/data/FD-catalog.json')
             detailsSticky.appendChild(detailsTitle);
 
             // description
-            // let detailsDescription = document.createElement('p');
-            // detailsDescription.innerText = item.Description;
-            // detailsDescription.classList.add('details-description');
-            // detailsDiv.appendChild(detailsDescription);
             let descriptionText = item.Description.split('<br>');
             let detailsDescription = document.createElement('p');
             detailsDescription.classList.add('details-description');
 
             descriptionText.forEach((line, index) => {
-                // Create a text node for each line
                 detailsDescription.appendChild(document.createTextNode(line));
                 
-                // Add a line break after each line except the last one
                 if (index < descriptionText.length - 1) {
                     detailsDescription.appendChild(document.createElement('br'));
                 }
